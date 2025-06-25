a943968e25465e9ed949af75e961832f7d2fd633
FelipeMendez346 <felipemendez343@gmail.com>
2025-06-24 23:59:50 -0400

    1

--- proyectofinal.py
+++ proyectofinal.py
@@ -1,1 +1,41 @@
-#quefasfaswds ssd
+
+
+class Bacteria():
+    def __init__(self):
+        self.id=None
+        self.raza=None
+        self.energia=0
+        self.resistente=False
+        self.estado=False
+
+    def Alimentar(self):
+        
+
+class Ambiente():
+    def __init__(self):
+        self.grilla=None
+        self.nutrientes=0
+        self.factor_ambiental=None
+
+    def actualizar_nutrientes(self):
+
+    def difundir_nutrientes(self):
+
+    def aplicar_ambiente(self):
+
+
+class Colonia():
+    def __init__(self):
+        self.Bacterias=[]
+        self.Ambiente=None
+
+    def paso():
+
+    def reporte_estado():
+
+    def exportar_csv():
+    
+
+#main    
+    
+
